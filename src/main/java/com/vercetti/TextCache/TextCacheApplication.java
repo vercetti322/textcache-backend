9c46f47814a8750d8105a368a8c88651b8feeeff
vercetti322 <vercettitommy322@gmail.com>
2024-09-08 17:58:57 +0530

    init

---
+++ src/main/java/com/vercetti/TextCache/TextCacheApplication.java
@@ -0,0 +1,13 @@
+package com.vercetti.TextCache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vercetti/TextCache/TextCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vercetti.TextCache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
